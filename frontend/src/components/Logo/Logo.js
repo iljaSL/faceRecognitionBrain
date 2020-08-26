4b6e442613c742ac5627793e727f31b1ce8e2cb5
Ilja Smelich <ismelich@c2r6p7.hive.fi>
2020-08-26 14:13:16 +0300

    refactored and connected to frontend

---
+++ frontend/src/components/Logo/Logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Tilt from "react-tilt";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3">
+          <img
+            alt="logo"
+            src="https://img.icons8.com/carbon-copy/100/000000/brain.png"
+          />{" "}
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
